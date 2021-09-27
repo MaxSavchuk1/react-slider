40f54e540c5e08385079a554c08832478b138393
MaxSavchuck <avatar2945@gmail.com>
2021-09-27 15:19:20 +0300

    start develop slider, add some logic and styles

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { images } from './../../api/images';
 import styles from './Slider.module.sass';
 
 function Slider () {
-  return <div></div>;
+  const [currentSlideNumber, setCurrentSlideNumber] = useState(0);
+  let currentSlide = images[currentSlideNumber];
+  const nextButtonHandler = () => {
+    currentSlideNumber === images.length - 1
+      ? setCurrentSlideNumber(0)
+      : setCurrentSlideNumber(currentSlideNumber + 1);
+  };
+  const prevButtonHandler = () => {
+    currentSlideNumber === 0
+      ? setCurrentSlideNumber(images.length - 1)
+      : setCurrentSlideNumber(currentSlideNumber - 1);
+  };
+  return (
+    <div className={styles.slidesContainer}>
+      <button className={styles.changeSlide} onClick={prevButtonHandler}>
+        &#10094;
+      </button>
+      <img src={currentSlide} alt='slide :)' />
+      <button className={styles.changeSlide} onClick={nextButtonHandler}>
+        &#10095;
+      </button>
+    </div>
+  );
 }
 
 export default Slider;
 
-//  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-//  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+//  <a class="prev" onclick="plusSlides(-1)"></a>
+//  <a class="next" onclick="plusSlides(1)"></a>
+
 // кнопка: nextSlide,
 // кнопка: prevSlide,
 // play/pause - auto timeout,
