ebe7674c4979d5adb7737c1e33d8b5a58cd5678a
MaxSavchuk <avatar2945@gmail.com>
2021-09-28 23:06:02 +0300

    done...?

--- src/components/Slider/images.js
+++ src/components/Slider/images.js
@@ -6,4 +6,6 @@ export const images = [
   'https://bipbap.ru/wp-content/uploads/2017/04/11-1.jpg',
   'http://fotorelax.ru/wp-content/uploads/2016/03/Beautiful-photos-and-pictures-on-different-topics-01.jpg',
   'https://a.d-cd.net/loAAAgOLq-A-960.jpg',
+  'https://miro.medium.com/max/2732/1*qvphkxaDna8aw0zZ8-fx5A.jpeg',
+  'http://www.rosphoto.com/images/u/articles/1601/4-sergey-sutkovoy.jpg',
 ];
--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,52 +1,49 @@
 import classNames from 'classnames';
 import React, { useEffect, useState } from 'react';
-import { images } from './../../api/images';
+import { images } from './images.js';
 import styles from './Slider.module.sass';
 
 function Slider () {
-  const [currentSlideNumber, setCurrentSlideNumber] = useState(0); //определяю номер слайда
-  const [duration, setDuration] = useState(5000); // интервал смены слайдов
-  const [isPaused, setIsPaused] = useState(false); // на паузе или нет
+  const [currentSlideNumber, setCurrentSlideNumber] = useState(0);
+  const [duration, setDuration] = useState(5000);
+  const maxDuration = 10000;
+  const [isPaused, setIsPaused] = useState(false);
   const [isFullScreen, setIsFullScreen] = useState(false);
   const durationHandler = ({ target: { value } }) => {
-    // инпут меняет длину задержки между слайдами
     setDuration(value);
   };
 
-  let currentSlide = images[currentSlideNumber]; // текущий номер слайда в рендере
+  let currentSlide = images[currentSlideNumber];
 
   const nextSlideHandler = () => {
-    // следующий слайд
     currentSlideNumber === images.length - 1
       ? setCurrentSlideNumber(0)
       : setCurrentSlideNumber(currentSlideNumber + 1);
   };
   const prevSlideHandler = () => {
-    // предыдущий слайд
     currentSlideNumber === 0
       ? setCurrentSlideNumber(images.length - 1)
       : setCurrentSlideNumber(currentSlideNumber - 1);
   };
 
   const controlHandler = () => {
-    // старт и пауза
     setIsPaused(!isPaused);
-    duration > 10000 ? setDuration(5000) : setDuration(999999); // ставлю 16 минут если пауза :)
+    duration > maxDuration ? setDuration(5000) : setDuration(9999999);
   };
 
   const fullScreenHandler = () => {
-    // при нажатии на фул скрин анимация ставится на паузу
     setIsFullScreen(!isFullScreen);
-    controlHandler();
+    if (!isPaused) {
+      controlHandler();
+    }
   };
 
   const fullScreenStyle = classNames(styles.fullScreen, {
-    //стили фул скрин
     [styles.fullScreenOn]: isFullScreen,
+    [styles.emergence]: isFullScreen,
   });
 
   useEffect(() => {
-    // собственно интервал смены слайдов
     const _ = setInterval(nextSlideHandler, duration);
     return () => {
       clearInterval(_);
@@ -55,50 +52,62 @@ function Slider () {
 
   return (
     <>
-      <div className={styles.slidesContainer}>
-        <button className={styles.changeSlide} onClick={prevSlideHandler}>
-          &#10094;
-        </button>
-        <div className={styles.imageContainer}>
-          <img src={currentSlide} alt='slide' />
-        </div>
-
-        <button className={styles.changeSlide} onClick={nextSlideHandler}>
-          &#10095;
-        </button>
-      </div>
-      <div className={styles.controlPanel}>
-        {isPaused ? (
-          <button onClick={controlHandler}>
-            <i className='fas fa-play'></i>
-          </button>
-        ) : (
-          <button onClick={controlHandler}>
-            <i className='fas fa-pause'></i>
-          </button>
-        )}
-        {!isPaused && (
-          <>
-            <span>
-              Interval{' '}
-              {duration / 1000 + ' second' + (duration > 1000 ? 's' : '')}
-            </span>
-            <input
-              type='range'
-              name='interval'
-              min='1000'
-              max='10000'
-              value={duration}
-              step='500'
-              onChange={durationHandler}
-            />
-          </>
-        )}
-        <button onClick={fullScreenHandler}>
-          <i className='fas fa-expand'></i>
-        </button>
-      </div>
+      {!isFullScreen && (
+        <>
+          <div className={styles.slidesContainer}>
+            <button
+              className={styles.changeSlideButton}
+              onClick={prevSlideHandler}
+            >
+              &#10094;
+            </button>
+            <div className={styles.imageContainer}>
+              <img src={currentSlide} alt='slide' />
+            </div>
+            <button
+              className={styles.changeSlideButton}
+              onClick={nextSlideHandler}
+            >
+              &#10095;
+            </button>
+          </div>
+          <div className={styles.controlPanel}>
+            {isPaused ? (
+              <button onClick={controlHandler}>
+                <i className='fas fa-play'></i>
+              </button>
+            ) : (
+              <button onClick={controlHandler}>
+                <i className='fas fa-pause'></i>
+              </button>
+            )}
+            {!isPaused && (
+              <>
+                <div className={styles.intervalSet}>
+                  <span>
+                    Interval:{' '}
+                    {duration / 1000 + ' second' + (duration > 1000 ? 's' : '')}
+                  </span>
+                  <input
+                    type='range'
+                    name='interval'
+                    min='1000'
+                    max={maxDuration}
+                    value={duration}
+                    step='1000'
+                    onChange={durationHandler}
+                  />
+                </div>
+              </>
+            )}
+            <button onClick={fullScreenHandler}>
+              <i className='fas fa-expand'></i>
+            </button>
+          </div>
+        </>
+      )}
       <div className={fullScreenStyle} onClick={fullScreenHandler}>
+        <span>Click anywhere to close</span>
         <img src={currentSlide} alt='slide' />
       </div>
     </>
